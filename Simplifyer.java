503d12b53ac5a6940c67f836b39388e022335fa5
Nathan Weel <nathanweel09@gmail.com>
2022-02-21 19:28:22 -0600

    Made some small fixes

--- Simplifyer.java
+++ Simplifyer.java
@@ -4,20 +4,15 @@ public class Simplifyer {
         //I got the main code to simplify from the URL below
         // https://stackoverflow.com/questions/6618994/simplifying-fractions-in-java
 
-        System.out.println("                                                \n" +
-                " \\    / _  |  _  _  ._ _   _                     \n" +
-                "  \\/\\/ (/_ | (_ (_) | | | (/_                    \n" +
-                " |_)      ._   _. _|_ |_   _. ._        _   _  | \n" +
-                " |_) \\/   | | (_|  |_ | | (_| | | \\/\\/ (/_ (/_ | \n" +
-                "     /                                           ");
+        System.out.println("                                                \n" + " \\    / _  |  _  _  ._ _   _                     \n" + "  \\/\\/ (/_ | (_ (_) | | | (/_                    \n" + " |_)      ._   _. _|_ |_   _. ._        _   _  | \n" + " |_) \\/   | | (_|  |_ | | (_| | | \\/\\/ (/_ (/_ | \n" + "     /                                           ");
 
-        System.out.println("What is your numerator (the fraction cannot be an improper fraction or it breaks the program)");
+        System.out.println("What is your numerator?" + "\n(this number cannot exceed or be equal to the next number you enter or it breaks the program)");
         int n1 = Integer.parseInt(System.console().readLine());
 
         System.out.println("What is your denominator?");
         int n2 = Integer.parseInt(System.console().readLine());
 
-        if (n1 > n2){
+        if (n1 >= n2) {
             System.out.println("Improper fractions cannot be simplified with this program");
             System.exit(0);
         }
@@ -33,8 +28,6 @@ public class Simplifyer {
         int n3 = simp1 / n1;
         int n4 = simp2 / n1;
 
-        System.out.println("Your simplified fraction is:");
-
-        System.out.print(n3 + "/" + n4 + "\n\n");
+        System.out.println("Your simplified fraction is:" + n3 + "/" + n4);
     }
 }
